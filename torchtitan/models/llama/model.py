921435f224283662ca2524cdbda4a6a738cf669f
Alper Canberk <alper.tu.canberk@gmail.com>
2024-11-17 17:37:28 -0500

    set is_causal to false

--- torchtitan/models/llama/model.py
+++ torchtitan/models/llama/model.py
@@ -207,7 +207,7 @@ class Attention(nn.Module):
         xv = values.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
 
         # we use casual mask for training
-        output = F.scaled_dot_product_attention(xq, xk, xv, is_causal=True)
+        output = F.scaled_dot_product_attention(xq, xk, xv, is_causal=False)
         output = output.transpose(
             1, 2
         ).contiguous()  # (bs, seqlen, n_local_heads, head_dim)
